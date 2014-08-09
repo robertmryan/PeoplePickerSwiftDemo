61f8a8f6844a94268828ad5285ec8d3e80c318d4
robertmryan <robert.ryan@mindspring.com>
2014-08-09 19:30:27 -0400

    Initial Commit

---
+++ PeoplePickerSwiftDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PeoplePickerSwiftDemo
+//
+//  Created by Robert Ryan on 8/9/14.
+//  Copyright (c) 2014 Robert Ryan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
